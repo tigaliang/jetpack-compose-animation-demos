ca9185b2d1774027d1621d744bff07f3c7f75203
Tiga Liang <liangkangabc@gmail.com>
2022-11-01 22:36:03 +0800

    update demo display title

--- app/src/main/java/com/tigaontech/compose/animation/screens/AnimationDemo.kt
+++ app/src/main/java/com/tigaontech/compose/animation/screens/AnimationDemo.kt
@@ -3,11 +3,11 @@ package com.tigaontech.compose.animation.screens
 enum class AnimationDemo(
     val displayName: String
 ) {
-    AnimateStates("通过改变某个属性实现动画"),
-    AnimatedVisibility("显示-隐藏的动画"),
-    AnimateContentSize("组件的内容大小发生变化时的动画"),
-    LazyListItemChanges("列表项发生变化时的动画"),
-    AnimatedContent("不同组件切换的动画")
+    AnimateStates("animate*AsState"),
+    AnimatedVisibility("AnimatedVisibility"),
+    AnimateContentSize("animateContentSize"),
+    LazyListItemChanges("animateItemPlacement"),
+    AnimatedContent("AnimatedContent")
     ;
 
     companion object {
