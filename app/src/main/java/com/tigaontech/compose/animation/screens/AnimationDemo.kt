6bbc058acee3b548a3597cce6afd422ce06a1849
Tiga Liang <liangkangabc@gmail.com>
2022-11-01 22:33:09 +0800

    update demo display title

--- app/src/main/java/com/tigaontech/compose/animation/screens/AnimationDemo.kt
+++ app/src/main/java/com/tigaontech/compose/animation/screens/AnimationDemo.kt
@@ -5,8 +5,9 @@ enum class AnimationDemo(
 ) {
     AnimateStates("通过改变某个属性实现动画"),
     AnimatedVisibility("显示-隐藏的动画"),
-    AnimateContentSize("UI 大小发生改变时的动画"),
+    AnimateContentSize("组件的内容大小发生变化时的动画"),
     LazyListItemChanges("列表项发生变化时的动画"),
+    AnimatedContent("不同组件切换的动画")
     ;
 
     companion object {
